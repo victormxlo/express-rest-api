e711ed7d5b3b04f481d054c750dbd941e37a9d28
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-05-24 10:53:26 -0300

    Registration route released (dist)

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = new (0, _express.Router)();
 // router.get('/', userController.index); // TBR
 // router.get('/:id', userController.show); // TBR
 
-router.post('/', _loginRequired2.default, _UserController2.default.store);
+router.post('/', _UserController2.default.store);
 router.put('/', _loginRequired2.default, _UserController2.default.update);
 router.delete('/', _loginRequired2.default, _UserController2.default.delete);
 
