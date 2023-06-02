e734654cd075808ce2156d56c8e8cca8161d478e
Victor Mello <joaovictortrajanomelo@gmail.com>
2023-06-02 16:50:57 -0300

    express-delay added

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import './database';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import delay from 'express-delay';
 
 import { resolve } from 'path';
 import homeRoutes from './routes/homeRoutes';
@@ -39,6 +40,7 @@ class App {
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
+    this.app.use(delay(2000));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
